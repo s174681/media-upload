f54ba7c921d5befa6759aced5263e47dcad25075
EC2 Default User <ec2-user@ip-172-31-20-4.eu-central-1.compute.internal>
2017-04-19 16:02:32 +0000

    add s3 storage to web

--- web/app.py
+++ web/app.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 from flask import request, redirect, url_for, render_template
 
+from media.s3_storage import S3MediaStorage
+
+import boto3
+
+from botocore.client import Config 
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,7 +17,9 @@ def upload():
 @app.route("/upload", methods=["POST"])
 def upload_file():
     uploaded_file = request.files['uploaded_media']
-    uploaded_file.save('/home/ec2-user/media-upload/var/files/' + uploaded_file.filename)
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    media_storage = S3MediaStorage(s3=s3, bucket_name='kanclerj-153')
+    media_storage.store(key=uploaded_file.filename, media=uploaded_file) 
     return redirect(url_for('upload'))
 
 if __name__ == "__main__":
