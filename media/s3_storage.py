38fb1f9df5241286ff4634e87c65361c9905a1b3
Ubuntu <ubuntu@ip-172-31-16-43.eu-central-1.compute.internal>
2017-06-07 16:07:20 +0000

    add store public to s3 when add animation

--- media/s3_storage.py
+++ media/s3_storage.py
@@ -13,3 +13,8 @@ class S3MediaStorage:
     bucket.download_file(key, '/tmp/%s' % item_uuid)
    
     return open('/tmp/%s' % item_uuid, 'r')
+  
+  def store_public(self, key, media):
+    bucket = self._s3.Bucket(self._bucket_name)
+    bucket.put_object(Key=key, Body=media, ACL='public-read')
+
--- tests/media/test_s3_media_storage.py
+++ tests/media/test_s3_media_storage.py
@@ -3,6 +3,8 @@ import boto3
 import os
 from botocore.client import Config
 from media.s3_storage import S3MediaStorage
+import uuid
+import requests
 
 class TestS3MediaStorage(unittest.TestCase):
   def test_upload_file_to_s3(self):
@@ -17,6 +19,17 @@ class TestS3MediaStorage(unittest.TestCase):
     media_storage = S3MediaStorage(s3=s3, bucket_name='kanclerj')
     media = media_storage.get(key='upl_name.txt')
     assert media.read(10000) == 'alfa omega\n'  
+  
+  def test_public_access_to_s3(self):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    to_upld_path = os.path.join(os.path.dirname(__file__), 'test.txt')
+    media_storage = S3MediaStorage(s3=s3, bucket_name='kanclerj')
+    name = str(uuid.uuid4())
+    to_upl_file = open(to_upld_path, 'rb')
+    media_storage.store_public(key=name, media=to_upl_file)
+    url = "https://s3.eu-central-1.amazonaws.com/kanclerj/%s" % name
+    r = requests.head(url)
+    assert r.status_code == 200
 
 if __name__ == '__main__':
   unittest.main()
